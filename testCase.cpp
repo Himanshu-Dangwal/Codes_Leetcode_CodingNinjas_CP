cc6a33e21207b5c912370acb9f1fd1385b251a76
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2023-08-28 22:38:03 +0530

    Some graphs problem

---
+++ testCase.cpp
@@ -0,0 +1,11 @@
+#include "bits/stdc++.h"
+using namespace std;
+int main()
+{
+    cout << 1000000 << endl;
+    for (int i = 0; i < 1000000; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
